04cf19bbf23dcbd56458ace1643e424d3cc5f302
SANJITH KUMAR R <sanjithkumar986@gmail.com>
2024-01-07 12:50:59 +0530

    changes_to_mod_arc0701

--- Basline_U_Net_Model.py
+++ Basline_U_Net_Model.py
@@ -13,16 +13,16 @@ class UNet(nn.Module):
         self.in_channel = inp_channels
         self.out_channels = out_channels
 
-        self.initial = ConvBlock(inp_channels, 64)
-        self.contract1 = EncodeDown(64, 128)
-        self.contract2 = EncodeDown(128, 256)
-        self.contract3 = EncodeDown(256, 512)
-        self.contract4 = EncodeDown(512, 1024)
-        self.expand1 = DecodeUp(1024,512)
-        self.expand2 = DecodeUp(512, 256)
-        self.expand3 = DecodeUp(256, 128)
-        self.expand4 = DecodeUp(128, 64)
-        self.resConv = ResultConv(64, out_channels)
+        self.initial = ConvBlock(inp_channels, 32)
+        self.contract1 = EncodeDown(32, 64)
+        self.contract2 = EncodeDown(64, 128)
+        self.contract3 = EncodeDown(128, 256)
+        self.contract4 = EncodeDown(256, 256)
+        self.expand1 = DecodeUp(512,128)
+        self.expand2 = DecodeUp(256, 64)
+        self.expand3 = DecodeUp(128, 32)
+        self.expand4 = DecodeUp(64, 32)
+        self.resConv = ResultConv(32, out_channels)
 
     def forward(self, x):
 
@@ -36,17 +36,17 @@ class UNet(nn.Module):
         x = self.expand3(x, x2)
         x = self.expand4(x, x1)
         x = self.resConv(x)
-        
+
         return x
-    
+
     def use_checkpointing(self):
-        self.initial = torch.utils.checkpoint(self.initial)
-        self.contract1 = torch.utils.checkpoint(self.contract1)
-        self.contract2 = torch.utils.checkpoint(self.contract2)
-        self.contract3 = torch.utils.checkpoint(self.contract3)
-        self.contract4 = torch.utils.checkpoint(self.contract4)
-        self.expand1 = torch.utils.checkpoint(self.expand1)
-        self.expand2 = torch.utils.checkpoint(self.expand2)
-        self.expand3 = torch.utils.checkpoint(self.expand3)
-        self.expand4 = torch.utils.checkpoint(self.expand4)
-        self.resConv = torch.utils.checkpoint(self.resConv)
+        self.initial = torch.utils.checkpoint.checkpoint(self.initial)
+        self.contract1 = torch.utils.checkpoint.checkpoint(self.contract1)
+        self.contract2 = torch.utils.checkpoint.checkpoint(self.contract2)
+        self.contract3 = torch.utils.checkpoint.checkpoint(self.contract3)
+        self.contract4 = torch.utils.checkpoint.checkpoint(self.contract4)
+        self.expand1 = torch.utils.checkpoint.checkpoint(self.expand1)
+        self.expand2 = torch.utils.checkpoint.checkpoint(self.expand2)
+        self.expand3 = torch.utils.checkpoint.checkpoint(self.expand3)
+        self.expand4 = torch.utils.checkpoint.checkpoint(self.expand4)
+        self.resConv = torch.utils.checkpoint.checkpoint(self.resConv)
